f5c3bda3839009669e2e97b9f5b431f39426bfd2
Eun Joo <eunjoolee787@gmail.com>
2015-02-10 21:48:38 -1000

    set up postgres but still need to fix errors

--- app/models/traffic_data.js
+++ app/models/traffic_data.js
@@ -1,56 +1,40 @@
-#!/usr/bin/env node
+var sequelize = require('./index.js').sequelize;
 
-var debug = require('debug')('express-example');
-var app = require('../app');
-var models = require("../models");
-
-app.set('port', process.env.PORT || 3000);
-
-models.sequelize.sync().then(function () {
-  var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
-  });
-});
-
-//sequelize
-var sequelize = require('./traffic_data.js');
-
-var Sequelize = require ('sequelize')
-  , sequelize = new Sequelize('area', 'location', 'address', 'code', 'type', 'date');
-
-var Data = sequelize.define('traffic_data', {
-  area: Sequelize.STRING,
-  location: Sequelize.STRING,
-  address: Sequelize.STRING,
-  code: Sequelize.STRING,
-  type: Sequelize.STRING,
-  date: Sequelize.DATE,
-  latitude: {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-    defaultValue: null,
-    validate: { min: -90, max: 90 }
-  },
-  longitude: {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-    defaultValue: null,
-    validate: { min: -180, max: 180 }
-  },
-}, {
-  validate: {
-    bothCoordsOrNone: function () {
-      if ((this.latitude === null) === (this.longitude === null)) {
-        throw new Error ('Require either both latitude and longitude or neither');
+module.exports = function(sequelize, Sequelize) {
+  var Data = sequelize.define('traffic_data', {
+    area: Sequelize.STRING,
+    location: Sequelize.STRING,
+    address: Sequelize.STRING,
+    code: Sequelize.STRING,
+    type: Sequelize.STRING,
+    date: Sequelize.DATE,
+    latitude: {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+      defaultValue: null,
+      validate: { min: -90, max: 90 }
+    },
+    longitude: {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+      defaultValue: null,
+      validate: { min: -180, max: 180 }
+    },
+  }, {
+    validate: {
+      bothCoordsOrNone: function () {
+        if ((this.latitude === null) === (this.longitude === null)) {
+          throw new Error ('Require either both latitude and longitude or neither');
+        }
       }
     }
-  }
-});
-
+  });
+  return Data;
+};
 
 
 // //Tester ONLY
-// return sequelize.sync().then(function() {
+// sequelize.sync().then(function() {
 //   return Data.create({
 //     area: "KANEOHE",
 //     location: "PALI TUNNELS D4 S",
@@ -58,12 +42,10 @@ var Data = sequelize.define('traffic_data', {
 //     code: "633",
 //     type: "STALLED/HAZARDOUS VEHICLE",
 //     date: new Date(2015, 12, 12)
-//   }).then(function(sdepold) {
-//     console.log(sdepold.values);
+//   }).then(function(data) {
+//     console.log(data.values);
 //   });
 // });
-
-// module.exports = model;
 
 //RUN ON TERMINAL ONLY
 // pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
--- server.js
+++ server.js
@@ -2,44 +2,30 @@ var express = require('express');
 var app = express();
 
 var models = require('./app/models/traffic_data');
-
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('database_name', 'username', 'password', {
-  disalect: "mysql",
-  port: 5432,
-});
+require('./app/models/index');
 
 sequelize
   .authenticate()
   .complete(function(err) {
     if (!!err) {
-      console.log('Unable to connect to the database:', err)
+      console.log('Unable to connect to the database:', err);
     } else {
-      console.log('Connection has been established successfully.')
+      console.log('Connection has been established successfully.');
     }
   });
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port);
 
 });
-// var debug = require('debug')('express-example');
-// var app = require('../app');
-// var models = require("../models");
 
-// app.set('port', process.env.PORT || 3000);
 
-// models.sequelize.sync().then(function () {
-//   var server = app.listen(app.get('port'), function() {
-//     debug('Express server listening on port ' + server.address().port);
-//   });
-// });
