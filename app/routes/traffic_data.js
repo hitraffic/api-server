cf408705ee376eb1e96ec6e1dbd1f21920437bb9
Te Vallee <astronomemusic@gmail.com>
2015-02-11 19:59:10 -1000

    f

--- app/routes/traffic_data.js
+++ app/routes/traffic_data.js
@@ -1,10 +1,2 @@
-var models = require('../models');
-var express = require('express');
-var router = express.Router();
-var Sequelize = require('sequelize');
+"use strict";
 
-router.get('/', function(req, res) {
-  
-});
-
-module.exports = router;
