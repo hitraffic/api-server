105bcf4c5b5093b222197b954e94738421762a5f
Te Vallee <astronomemusic@gmail.com>
2015-02-21 13:27:58 -1000

    merge

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ var express = require('express');
 var app = express();
 var request = ('request');
 
+
+var models = require('./app/models/traffic_data');
+require('./app/models/index');
+
+
 app.get('/', function (req, res) {
+
   res.send('Hello World');
 });
 
-
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port);
+
 
 });
+
+
