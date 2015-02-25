bb7ceae06f898ed5432066c38f3b68a6cec31503
Eun Joo <eunjoolee787@gmail.com>
2015-02-24 19:22:05 -1000

    create mock_data url

--- app/models/index.js
+++ app/models/index.js
@@ -1,107 +1,67 @@
 "use strict";
 
-// var fs        = require("fs");
+var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require('../../config/config.json')[env];
-console.log(config);
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 var Data = require('./traffic_data.js');
-var mockData = require('./mock_data.js');
-console.log(Data);
+// var mockData = require('./mock_data.js');
 
-sequelize
-  .authenticate()
-  .complete(function(err) {
-    if (!!err) {
-      console.log('Unable to connect to the database:', err);
-    } else {
-      console.log('Connection has been established successfully.');
-    }
-  });
 
-var incident = sequelize.define('incident', {
-    id: Sequelize.INTEGER,
-    date: Sequelize.DATE,
-    code: Sequelize.INTEGER,
-    type: Sequelize.STRING,
-    address: Sequelize.STRING,
-    location: Sequelize.STRING,
-    area: Sequelize.STRING,
-    lat: {
-      type: Sequelize.FLOAT,
-      allowNull: true,
-      defaultValue: null,
-      validate: { min: -90, max: 90 }
-    },
-    lng: {
-      type: Sequelize.FLOAT,
-      allowNull: true,
-      defaultValue: null,
-      validate: { min: -180, max: 180 }
-    },
-  // }, 
-  // {
-  //   validate: {
-  //     bothCoordsOrNone: function (value) {
-  //       console.log(this);
-  //       if ((value.lat === null) === (value.lng === null)) {
-  //         throw new Error ('Require either both latitude and longitude or neither');
-  //       }
-  //     }
-  //   }
-  });
-
-sequelize
-  .sync({ force: true })    // drops all tables everytime we run
-  .complete(function(err) {
-    if (!!err) {
-      console.log('An error occurred while creating the table:', err);
-    } else {
-      for(var i = 0; i < mockData.length; i++) {
-        incident
-        .create({
-          item: mockData[i].item,
-          date: mockData[i].date,
-          code: mockData[i].code,
-          type: mockData[i].type,
-          address: mockData[i].address,
-          location: mockData[i].location,
-          area: mockData[i].area,
-          latitude: mockData[i].latitude,
-          longitude: mockData[i].longitude
-        })
-        .complete(function(err, user) {
-          if (!!err) {
-            console.log('The instance has not been saved:', err);
-          } else {
-            console.log('We have a persisted instance now');
-          }
-        });
-      console.log('It worked!');
-    }
-  }
-});
-// fs
-//   .readdirSync(__dirname)
-//   .filter(function(file) {
-//     return (file.indexOf(".") !== 0) && (file !== basename);
-//   })
-//   .forEach(function(file) {
-//     var model = sequelize["import"](path.join(__dirname, file));
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach(function(modelName) {
-//   if ("associate" in db[modelName]) {
-//     db[modelName].associate(db);
+// sequelize
+//   .sync({ force: true })    // drops all tables everytime we run
+//   .complete(function(err) {
+//     if (!!err) {
+//       console.log('An error occurred while creating the table:', err);
+//     } else {
+//       for(var i = 0; i < mockData.length; i++) {
+//         incident
+//         .create({
+//           item: mockData[i].item,
+//           date: mockData[i].date,
+//           code: mockData[i].code,
+//           type: mockData[i].type,//TO DO
+//           address: mockData[i].address,
+//           location: mockData[i].location,//TO DO
+//           area: mockData[i].area,//TO DO
+//           latitude: mockData[i].latitude,
+//           longitude: mockData[i].longitude
+//         })
+//         .complete(function(err, user) {
+//           if (!!err) {
+//             console.log('The instance has not been saved:', err);
+//           } else {
+//             console.log('We have a persisted instance now');
+//           }
+//         });
+//       console.log('It worked!');
+//     }
 //   }
 // });
-//TO DO
-// db.Data.sync({ force: true}).success(function() {
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== basename);
+  })
+  .forEach(function(file) {
+    var model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+// db.Data
+//   .sync({ force: true})
+//   .success(function() {
 //     console.log('Hello');
 //     Data.bulkCreate(mockData).success(function() {
 //       console.log("success");
--- app/routes/traffic_data.js
+++ app/routes/traffic_data.js
@@ -2,12 +2,54 @@ var models = require('../models');
 var express = require('express');
 var router = express.Router();
 var Sequelize = require('sequelize');
-var mockData = require('../models/mock_data.js');
 var incident = require('../models/traffic_data');
+
 //EJL-Incident
-router.get('/', function(req, res) {
+router.get('/area/:area', function(req, res) {
+  console.log(req.param);
   models.incident
-    .findAll({where: {date: }})
-});
+    .findAll({
+      where: {
+        area: req.param("area")}
+      })
+    .then(function(incidents){
+      res.json(incidents);
+    });
+  });
+
+// sequelize.query("SELECT type FROM incident WHERE area = 'incident[i].area", 
+//   { replacements: ['active'], type: sequelize.QueryTypes.SELECT}
+//     ).then(function(incidentByArea) {
+//       console.log('incidents by area: ', incidentByArea);
+//   });
+
+// Session.find({
+//   where: ['user_id=? or token=? or expires > NOW()', someNumber, someString] 
+// }).on('success', function(s) {
+
+// });
+
+// Session.find({
+//   where: {id: incident},
+//   include: {type: mockData, location: mockData, area: mockData}
+// });
+
+// var Data = function (sequelize, Sequelize) {
+
+//   Data.findOrCreate({
+//     where: {
+//       type: mockData,
+//       location: mockData,
+//       area: mockData
+//     },
+//     defaults: {
+
+//     }
+//   }).spread(function(data, incident) {
+//     console.log(data.values);
+//   }).fail(function(err) {
+//     console.log('Error occured', err);
+//   });
+// };
 
 module.exports = router;
